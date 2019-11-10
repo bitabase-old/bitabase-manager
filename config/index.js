87188a270b607479449e51e60b5c87c541cdd1bd
Mark Wylde <me@markwylde.co.uk>
2019-11-10 12:06:11 +1000

    Add environment variables

--- config/index.js
+++ config/index.js
@@ -1,6 +1,18 @@
-module.exports = {
-  port: 8081,
-  allowedCrossOriginDomains: [
-    'http://localhost:8080'
-  ]
+const config = {
+  dev: {
+    port: 8081,
+    allowedCrossOriginDomains: [
+      'http://localhost:8080'
+    ]
+  },
+
+  production: {
+    port: 80,
+    allowedCrossOriginDomains: [
+      'https://bitabase.com',
+      'https://www.bitabase.com'
+    ]
+  }
 }
+
+module.exports = config[process.env.NODE_ENV || 'dev']
